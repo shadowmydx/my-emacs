4d7cd09c2dc1e1b5cffe0f03c5fe079be1507d4c
Light Wu <wuli5@oocl.com>
2016-05-18 11:31:15 +0800

    bugfix about pare in string

--- my-function.el
+++ my-function.el
@@ -21,11 +21,18 @@
   (cond ((= 1 (eval move-motion))
 	 (list 'decf target))
 	(t (list 'incf target))))
-
+(defun shadowmydx-skip-all-quote (cur-pos direction)
+  (save-excursion
+	(goto-char cur-pos) 
+    (if (= 1 direction)
+	(+ (re-search-backward "[^\\]\"") 1)
+      (- (re-search-forward "[^\\]\"") 1))))
 (defun shadowmydx-search-pair-pare (cur-pos start-count move-1 move-2)
   (shadowmydx-move move-1 cur-pos)
   (while (and (> start-count 0) (> cur-pos 0) (< cur-pos (point-max)))
-    (cond ((equal "(" (buffer-substring cur-pos (+ 1 cur-pos)))
+    (cond ((equal "\"" (buffer-substring cur-pos (+ 1 cur-pos)))
+	   (setq cur-pos (shadowmydx-skip-all-quote cur-pos move-1)))
+	  ((equal "(" (buffer-substring cur-pos (+ 1 cur-pos)))
 	   (shadowmydx-move move-1 start-count))
 	  ((equal ")" (buffer-substring cur-pos (+ 1 cur-pos)))
 	   (shadowmydx-move move-2 start-count)))
@@ -53,31 +60,32 @@
 
 (defun shadowmydx-detect-if-a-pare ()
   (interactive)
-  (if (not (= shadowmydx-current-pos (point)))
-      (progn
-	(setq shadowmydx-current-pos (point))
-	(let ((delete-pos (shadowmydx-pop-item shadowmydx-colored-point)))
-	   (if delete-pos
-	       (shadowmydx-change-pare-style delete-pos (+ 1 delete-pos) shadowmydx-current-background)))
-	(if (and (< shadowmydx-current-pos (point-max))
-		 (> shadowmydx-current-pos (point-min)))
-	    (cond ((equal "(" (buffer-substring (point) (+ 1 (point))))
-		   (let ((right-pare-pos (shadowmydx-search-pair-pare (point) 1 0 1)))
-		     (if (and
-			  (> right-pare-pos (point-min))
-			  (< right-pare-pos (point-max))
-			  (> right-pare-pos (+ 1 shadowmydx-current-pos)))
-			 (progn
-			   (shadowmydx-change-pare-style right-pare-pos (+ 1 right-pare-pos) shadowmydx-align-pare-background)
-			   (shadowmydx-append-to shadowmydx-colored-point right-pare-pos)))))
-		  ((equal ")" (buffer-substring (point) (+ 1 (point))))
-		   (let ((left-pare-pos (shadowmydx-search-pair-pare (point) 1 1 0)))
-		     (if (and
-			  (> left-pare-pos (point-min))
-			  (< left-pare-pos (point-max))
-			  (< left-pare-pos (- shadowmydx-current-pos 1)))
-			 (progn
-			   (shadowmydx-change-pare-style left-pare-pos (+ 1 left-pare-pos) shadowmydx-align-pare-background)
-			   (shadowmydx-append-to shadowmydx-colored-point left-pare-pos))))))))))
+  (ignore-errors
+    (if (not (= shadowmydx-current-pos (point)))
+	(progn
+	  (setq shadowmydx-current-pos (point))
+	  (let ((delete-pos (shadowmydx-pop-item shadowmydx-colored-point)))
+	    (if delete-pos
+		(shadowmydx-change-pare-style delete-pos (+ 1 delete-pos) shadowmydx-current-background)))
+	  (if (and (<= shadowmydx-current-pos (point-max))
+		   (>= shadowmydx-current-pos (point-min)))
+	      (cond ((equal "(" (buffer-substring (point) (+ 1 (point))))
+		     (let ((right-pare-pos (shadowmydx-search-pair-pare (point) 1 0 1)))
+		       (if (and
+			    (> right-pare-pos (point-min))
+			    (< right-pare-pos (point-max))
+			    (> right-pare-pos (+ 1 shadowmydx-current-pos)))
+			   (progn
+			     (shadowmydx-change-pare-style right-pare-pos (+ 1 right-pare-pos) shadowmydx-align-pare-background)
+			     (shadowmydx-append-to shadowmydx-colored-point right-pare-pos)))))
+		    ((equal ")" (buffer-substring (point) (+ 1 (point))))
+		     (let ((left-pare-pos (shadowmydx-search-pair-pare (point) 1 1 0)))
+		       (if (and
+			    (> left-pare-pos (point-min))
+			    (< left-pare-pos (point-max))
+			    (< left-pare-pos (- shadowmydx-current-pos 1)))
+			   (progn
+			     (shadowmydx-change-pare-style left-pare-pos (+ 1 left-pare-pos) shadowmydx-align-pare-background)
+			     (shadowmydx-append-to shadowmydx-colored-point left-pare-pos)))))))))))
 
 (add-to-list 'post-command-hook #'shadowmydx-detect-if-a-pare)
